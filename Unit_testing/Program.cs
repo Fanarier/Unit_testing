975c4a6b21cfc39e1e4cf7bb2b4e847888d3bca3
KP_349 <KP_349@FANARIER>
2023-10-22 16:13:48 +0200

    Добавьте файлы проекта.

---
+++ Unit_testing/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int N = 5;
+        int result = GetTotalDivisorsInRange(N);
+        Console.WriteLine(result);
+    }
+
+    private static int GetTotalDivisorsInRange(int N)
+    {
+        int totalDivisors = 0;
+
+
+        for (int i = N; i <= 2 * N; i++)
+        {
+
+            int divisors = 0;
+            for (int j = 1; j <= i; j++)
+            {
+                if (i % j == 0)
+                {
+                    divisors++;
+                }
+            }
+
+
+            totalDivisors += divisors;
+        }
+
+        return totalDivisors;
+    }
+}
